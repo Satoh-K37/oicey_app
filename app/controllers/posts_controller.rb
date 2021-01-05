5ddb19454721b1d453f1ed7b193e9438cde45807
Satoh-K37 <skohcode0385@gmail.com>
2021-01-06 01:31:32 +0900

    feat:新規投稿機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,8 +6,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params)
+    post.save!
+    redirect_to posts_url, notice: "投稿を送信しました"
+  end
+
 
   def edit
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :images, :visit_day)
+  end
+
 end
