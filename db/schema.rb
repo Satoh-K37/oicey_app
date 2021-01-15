d2dbad936eec337471a652bbba0b81972fc07c74
Satoh-K37 <skohcode0385@gmail.com>
2021-01-16 00:43:59 +0900

    feat:PostsテーブルとUsersテーブルを関連付け

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_052310) do
+ActiveRecord::Schema.define(version: 2021_01_15_153614) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2021_01_15_052310) do
     t.date "visit_day"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
