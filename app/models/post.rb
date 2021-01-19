b3b53fad05caf8b81013852e409d4d4390c8f508
Satoh-K37 <skohcode0385@gmail.com>
2021-01-19 19:46:11 +0900

    feat:新規投稿時と投稿編集にタグ付できるようにした

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
-  
+  ##### タグ機能 #####
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
+  #######################
+
+  ##### バリデーション #####
   # 現状は本文を必須にしてるけど、画像投稿機能を実装したら画像か本文のどちらかが投稿されてればOKって感じにしたい
   validates :body, presence: true ,length:  { maximum: 500 }
-  
+  #######################
 end
