d63155f1345e9cf49d6c7bce7b7b73224ef03acc
Satoh-K37 <skohcode0385@gmail.com>
2021-01-27 00:47:06 +0900

    feat: relationshipsコントローラーに必要なアクションを追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,30 @@
 class RelationshipsController < ApplicationController
+  before_action :set_user
+
+  # フォローするアクション
+  # Userモデルファイルで定義したfollow(other_user)インスタンスメソッドを使用
+  def create
+    following = current_user.follow(@user)
+    if following.save
+      redirect_to @user, notice: "フォローしました"
+    else
+      redirect_to @user, notice: "フォローできませんでした"
+  end
+
+  # フォロー解除するアクション
+  # Userモデルファイルで定義したunfollow(other_user)インスタンスメソッドを使用
+  def destroy
+    following = current_user.unfollow(@user)
+    if following.destroy
+      redirect_to @user, notice: "フォロー解除しました"
+    else
+      redirect_to @user, notice: "ユーザーのフォロー解除に失敗しました"
+    end
+  end
+
+  private
+  def set_user
+    # フォロー/フォロー解除ボタンからはparams[:relationship][:follow_id]という形でデータが送られてくるので次の形で受け取る
+    @user = User.find(params[:relationship][:follow_id])
+  end
 end
