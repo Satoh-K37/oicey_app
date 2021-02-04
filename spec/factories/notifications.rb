701bf3c87b1fc9cc323a731c4745e99ee65fd281
Satoh-K37 <skohcode0385@gmail.com>
2021-02-05 02:38:41 +0900

    feat: 通知機能の実装のためにモデル内容の

---
+++ spec/factories/notifications.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :notification do
+    visitor { nil }
+    visited { nil }
+    post { nil }
+    comment { nil }
+    action { "MyString" }
+    checked { "" }
+    g { "MyString" }
+    model { "MyString" }
+    Notification { "MyString" }
+    visitor { nil }
+    visited { nil }
+    post { nil }
+    comment { nil }
+    action { "MyString" }
+    checked { false }
+  end
+end
